b4581fc66cab7f41e5e71cbe35d795d85e558f7e
bgoclock <e1b21002.oit.ac.jp>
2023-09-26 15:49:55 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z2145/kaizi/janken/JankenApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z2145.kaizi.janken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenApplication.class, args);
+	}
+
+}
